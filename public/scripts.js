e2f1f6ec290a55871a62e8718faf797394cf36b0
stefankablowski <stefan.kablowski@gmail.com>
2020-03-27 06:35:01 +0100

    add fronend play button logic

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,5 @@
 var socket;
+var $ = (element) => document.querySelector(element);
 
 function createConnection() {
     if (socket) {
@@ -12,5 +13,19 @@ createConnection();
 socket.on('invitation-sent', function(userName, userId) {
     console.log(`${userName} wants to play with you! (userid:${userId})`);
 });
+
+function playRandom() {
+    console.log('wanna play random')
+    socket.emit('request_random_game');
+}
+
+function playFriend(roomname) {
+    socket.emit('request_friend_game', roomname);
+}
+
+
+// add event listener
+$('#play-random').addEventListener('click', playRandom);
+$('#play-friend').addEventListener('click', playFriend);
 
 localStorage.debug = 'socket.io-client:socket';
