4ef355aa81a8bdc64c0870406273cc794082e29b
stefankablowski <stefan.kablowski@gmail.com>
2020-03-27 09:20:34 +0100

    sanitize input and make toggle work

--- public/scripts.js
+++ public/scripts.js
@@ -19,14 +19,69 @@ function playRandom() {
     socket.emit('request_random_game');
 }
 
-function playFriend(roomname) {
-
-}
-
 //  after entering a room name
-function continuePlayFriend() {
+function playFriend(roomname) {
+    console.log(`i want to play in room ${roomname}`)
     socket.emit('request_friend_game', roomname);
 }
+
+/* Accept decline bar */
+const bar = {
+    show: () => {
+        $('#accept-decline-bar').classList.remove('hide');
+    },
+    hide: () => {
+        $('#accept-decline-bar').classList.add('hide');
+    },
+    accept: () => {
+        playFriend($('#name-input').value);
+    },
+    decline: () => {
+        roomInput.deleteContent();
+    }
+}
+
+const roomInput = {
+    checkContent: () => {
+        if ($('#name-input').value === '') {
+            bar.hide();
+        } else {
+            // sanitize input
+            $('#name-input').value = $('#name-input').value.replace(/[^a-zA-Z0-9]/g, '_');
+            if ($('#name-input').value.length > 16) {
+                $('#name-input').value = $('#name-input').value.slice(0, 16);
+            }
+            bar.show();
+        }
+    },
+    show: () => {
+        $('#enter-name-frame').classList.remove('hide');
+    },
+    hide: () => {
+        $('#enter-name-frame').classList.add('hide');
+        $('#accept-decline-bar').classList.add('hide');
+    },
+    toggle: () => {
+        if ($('#enter-name-frame').classList.contains('hide')) {
+            roomInput.show();
+            roomInput.focus();
+        } else {
+            roomInput.hide();
+            roomInput.deleteContent();
+        }
+    },
+    click: (event) => {
+        event.stopPropagation();
+    },
+    focus: () => {
+        $('#name-input').focus();
+    },
+    deleteContent: () => {
+        $('#name-input').value = '';
+    }
+}
+
+
 
 function accept() {
     console.log('accept');
@@ -37,10 +92,19 @@ function decline() {
 }
 
 
+function onPageLoad() {
+    roomInput.deleteContent();
+}
+
+$('#name-input').addEventListener('input', roomInput.checkContent);
+$('#name-input').addEventListener('click', roomInput.click);
+$('#accept').addEventListener('click', bar.accept);
+$('#decline').addEventListener('click', bar.decline);
+
 // add event listener
 $('#play-random').addEventListener('click', playRandom);
-$('#play-friend').addEventListener('click', playFriend);
-$('#accept').addEventListener('click', accept);
-$('#decline').addEventListener('click', decline);
+$('#play-friend').addEventListener('click', roomInput.toggle);
 
 localStorage.debug = 'socket.io-client:socket';
+
+onPageLoad();
